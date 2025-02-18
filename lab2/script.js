b96d9549a0e390eb0042410e69cc66871e22b969
manasarrafi <manastyles04@gmail.com>
2025-02-18 15:22:21 -0500

    updated lab 2

---
+++ lab2/script.js
@@ -0,0 +1,58 @@
+<script>
+// Object to control a moving shape
+class MovingShape {
+    constructor(x, y, size, speedX, speedY, color) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.speedX = speedX;
+        this.speedY = speedY;
+        this.color = color;
+    }
+
+    // Update position and check boundaries
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        // Bounce off walls
+        if (this.x < 0 || this.x > width) {
+            this.speedX *= -1;
+        }
+        if (this.y < 0 || this.y > height) {
+            this.speedY *= -1;
+        }
+    }
+
+    // Display the shape
+    show() {
+        fill(this.color);
+        ellipse(this.x, this.y, this.size);
+    }
+
+    // Change speed when clicked
+    changeSpeed() {
+        this.speedX = random(-5, 5);
+        this.speedY = random(-5, 5);
+    }
+}
+
+let shape;
+
+function setup() {
+    createCanvas(500, 400);
+    shape = new MovingShape(width / 2, height / 2, 50, 3, 2, 'blue');
+}
+
+function draw() {
+    background(240);
+    shape.update();
+    shape.show();
+}
+
+function mousePressed() {
+    shape.changeSpeed();
+}
+</script>
+</body>
+</html>
